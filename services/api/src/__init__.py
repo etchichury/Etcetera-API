fc4747a3e9af92df25a50fc3a465d482b498b958
etchichury <joaoetchichury@hotmail.com>
2020-05-27 00:40:29 -0300

    :art:: Add user blueprint

--- services/api/src/__init__.py
+++ services/api/src/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-from .to_do import to_do_blueprint
+from .routes import to_do, user
+
 
 app = Flask(__name__)
 app.config.from_object("src.config.Config")
-app.register_blueprint(to_do_blueprint)
+
+db = SQLAlchemy(app)
+
+app.register_blueprint(to_do.to_do_blueprint)
+app.register_blueprint(user.user_blueprint)
 
 
 db = SQLAlchemy(app)
